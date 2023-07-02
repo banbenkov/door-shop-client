0359883a8851401b85f22e0d3fc1b778243ac269
Ivan <banbenkov123@gmail.com>
2023-07-02 20:59:16 +0300

    Добавлен раздел избранные товары

--- src/actions/favor.js
+++ src/actions/favor.js
@@ -6,7 +6,7 @@ export const fetchFavor = (guestId) => {
     return async dispatch => {
         try {
             dispatch({type: 'FETCH_FAVOR_DOORS'})
-            const response = await axios.get(`http://localhost:5000/favor/${guestId}`)
+            const response = await axios.get(`https://dveri-arsenal.ru:444/favor/${guestId}`)
             dispatch({type: 'FETCH_FAVOR_DOORS_SUCCESS', payload: response.data})
         } catch (e) {
             dispatch({
@@ -20,7 +20,7 @@ export const fetchFavor = (guestId) => {
 export const sendFavor = (doorId, guestId) => {
     return async dispatch => {
         try {
-            const response = await axios.post(`http://localhost:5000/favor`, {doorId, guestId});
+            const response = await axios.post(`https://dveri-arsenal.ru:444/favor`, {doorId, guestId});
             if (response.data !== '') {
                 dispatch({type: 'ADD_FAVOR_DOORS', payload: response.data})
                 toast.success('Товар добавлен в избранное', {
@@ -40,7 +40,7 @@ export const sendFavor = (doorId, guestId) => {
 export const deleteFavor = (doorId, guestId) => {
     return async dispatch => {
         try {
-            const response = await axios.delete(`http://localhost:5000/favor`, {data: {doorId, guestId}})
+            const response = await axios.delete(`https://dveri-arsenal.ru:444/favor`, {data: {doorId, guestId}})
             dispatch({type: 'DELETE_FAVOR_DOORS', payload: doorId})
         } catch (e) {
 
--- src/actions/guest.js
+++ src/actions/guest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const sendGuest = (guest) => {
     return async dispatch => {
         try {
-            const response = await axios.post(`http://localhost:5000/guest`, guest)
+            const response = await axios.post(`https://dveri-arsenal.ru:444/guest`, guest)
         } catch (e) {
 
         }
